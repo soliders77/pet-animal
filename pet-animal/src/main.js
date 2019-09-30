ac51b0bcb3c7bd4b8c8662dd191de433cdf6db90
zhaoshuai <1556749161@qq.com>
2019-09-30 10:01:51 +0800

    11

--- pet-animal/src/main.js
+++ pet-animal/src/main.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Vant from 'vant';
+import 'vant/lib/index.css';
+import { Button } from 'vant'
 
 Vue.config.productionTip = false
-
+Vue.use(Vant)
+Vue.use(Button)
 new Vue({
   router,
   store,
--- pet-animal/src/router.js
+++ pet-animal/src/router.js
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-
+import global from './components/global/1.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-   
+   {path:"/global",component:global}
   ]
 })
